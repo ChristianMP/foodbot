f233efb2f3fbca99f3d28a8d70b81eebc6e84e39
Christian M. Pedersen <christian.moeller.pedersen@sallinggroup.com>
2024-01-15 09:51:53 +0100

    Fix Netlify function code to 2.0 functionality

--- netlify/functions/today.ts
+++ netlify/functions/today.ts
@@ -1,22 +1,7 @@
-import {Handler, HandlerEvent} from '@netlify/functions';
 import {main} from '../../src/today';
 
-const handler: Handler = async (event: HandlerEvent) => {
-  console.log('Received event:', event);
-
-  // Ensure DST corrected execution
-  const cphDate = new Date().toLocaleString('en-US', {
-    timeZone: 'Europe/Copenhagen',
-  });
-  if (new Date(cphDate).getHours() !== 9) {
-    console.log('Skipping due to DST correction');
-  } else {
-    await main();
-  }
-
-  return {
-    statusCode: 200,
-  };
+export default async (req: Request) => {
+  console.log('Received event:', req);
+  await main();
+  return new Response('OK');
 };
-
-export {handler};
